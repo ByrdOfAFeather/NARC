4e63246db613a06e6e72749eb5bfac15163f0b42
Matthew Byrd <matthew_a_byrd@outlook.com>
2017-12-12 14:30:12 -0500

    [Auto-Encoder] Switched to Hyperbolic Tangent activation function

--- ModelTraining/predictors.py
+++ ModelTraining/predictors.py
@@ -6,11 +6,6 @@ from datetime import datetime
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-log_root = 'logs'
-log_name = 'run-{}'.format(time)
-log_dir = '{}/{}/'.format(log_root, log_name)
-
 
 class AutoEncoder:
 	def __init__(self, data_set, test_data_set=None):
@@ -21,6 +16,11 @@ class AutoEncoder:
 
 	def run(self, learning_rate=.01, layer_1_f=40, layer_2_f=20, layer_3_f=10,
 	        epochs=1000, test_thresh=.3):
+		time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+		log_root = 'temp'
+		log_name = 'run-{}'.format(time)
+		log_dir = '{}/logs/{}/'.format(log_root, log_name)
+
 		input_features = self.data_set.shape[1]
 		weights = {
 			'encoder_h1': tf.Variable(tf.random_normal([input_features, layer_1_f])),
@@ -41,15 +41,15 @@ class AutoEncoder:
 		}
 
 		def encoder(X):
-			layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(X, weights['encoder_h1']), bias['encoder_b1']))
-			layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['encoder_h2']), bias['encoder_b2']))
-			layer_3 = tf.nn.sigmoid(tf.add(tf.matmul(layer_2, weights['encoder_h3']), bias['encoder_b3']))
+			layer_1 = tf.nn.tanh(tf.add(tf.matmul(X, weights['encoder_h1']), bias['encoder_b1']))
+			layer_2 = tf.nn.tanh(tf.add(tf.matmul(layer_1, weights['encoder_h2']), bias['encoder_b2']))
+			layer_3 = tf.nn.tanh(tf.add(tf.matmul(layer_2, weights['encoder_h3']), bias['encoder_b3']))
 			return layer_3
 
 		def decoder(X):
-			layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(X, weights['decoder_h1']), bias['decoder_b1']))
-			layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']), bias['decoder_b2']))
-			layer_3 = tf.nn.sigmoid(tf.add(tf.matmul(layer_2, weights['decoder_h3']), bias['decoder_b3']))
+			layer_1 = tf.nn.tanh(tf.add(tf.matmul(X, weights['decoder_h1']), bias['decoder_b1']))
+			layer_2 = tf.nn.tanh(tf.add(tf.matmul(layer_1, weights['decoder_h2']), bias['decoder_b2']))
+			layer_3 = tf.nn.tanh(tf.add(tf.matmul(layer_2, weights['decoder_h3']), bias['decoder_b3']))
 			return layer_3
 
 		data = tf.placeholder(dtype=tf.float32, shape=[None, input_features], name='data-set')
@@ -63,19 +63,25 @@ class AutoEncoder:
 		optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss)
 		init = tf.global_variables_initializer()
 
+		loggart_scaler = tf.summary.scalar("MSE", loss)
+		loggart = tf.summary.FileWriter(log_dir, tf.get_default_graph())
+
 		with tf.Session() as session:
 			session.run(init)
 			self.loss = None
 			test_list = None
 			anamoly_list = None
 
-			def train(x):
+			def train(x, log=False):
 				for i in range(0, epochs):
 					_, self.loss = session.run([optimizer, loss], feed_dict={data: x})
 					if i % 100 == 0:
 						print("STEP {} LOSS {}".format(i, self.loss))
+						if log:
+							summary_str = loggart_scaler.eval(feed_dict={data: x})
+							loggart.add_summary(summary_str, i)
 
-			train(self.data_set)
+			train(self.data_set, log=True)
 			if self.test_data_set.any:
 				test_list = []
 				anamoly_list = []
@@ -107,3 +113,4 @@ class AutoEncoder:
 						ex.write("USER ID {}\n".format(user_id))
 						ex.write("Anomaly MSE {}\n\n".format(MSE))
 				ex.close()
+			loggart.close()
