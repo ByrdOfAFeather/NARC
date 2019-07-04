6b82ce1c86ba3311d1e784120af0591300031838
ByrdOfAFeather <matthew_a_byrd@outlook.com>
2019-07-03 21:29:28 -0400

    Fixed static settings

--- NARC/settings.py
+++ NARC/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "/staticfiles/")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 STATICFILES_DIRS = (
   os.path.join(SITE_ROOT, 'static/'),
