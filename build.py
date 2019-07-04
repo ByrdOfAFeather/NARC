b485abb17872787c4ed30d0c64540f63ef86c646
ByrdOfAFeather <matthew_a_byrd@outlook.com>
2019-07-03 21:24:02 -0400

    Fixed requirements

--- build.py
+++ build.py
@@ -2,4 +2,10 @@ from pip._internal.operations.freeze import freeze
 
 with open('requirements.txt', 'w') as f:
 	for r in freeze():
-		f.write(r + "\n")
+		if "+mkl" in r:
+			# Numpy technically is not a requirement, but is extremely difficult to install on windows with +mkl
+			# so I have to not write it out here since it's technically not a separate package (installed whenever
+			# you have a compatible system)
+			f.write(r.split("+")[0] + "\n")
+		else:
+			f.write(r + "\n")
